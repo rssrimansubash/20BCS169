6584da5afd43ed1c7bc80b4a9a9d62c3911754db
rssrimansubash <rssrimansubash@gmail.com>
2023-07-05 12:11:06 +0530

    Files Upload

---
+++ src/App.js
@@ -0,0 +1,297 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p1>
+          All Train Schedules
+        </p1>
+        <br></br>
+        <table>
+          <thead>
+          <tr>
+            <th>
+              Train No.     
+            </th>
+            <th>
+              Train Name    
+            </th>
+            <th>
+              Arrival
+            </th>
+            <br></br>
+            <th>
+              Departure       
+            </th>
+          </tr>
+          </thead>
+          <br></br>
+          <tbody>
+          <tr>
+            <td>
+              16543
+            </td>
+            <td>
+              Mysore Express
+            </td>
+            <td>
+              17:15
+            </td>
+            <br></br>
+            <td>
+              19:15
+            </td>
+          </tr>
+          <tr>
+            <td>
+              16493
+            </td>
+            <td>
+              Pearl City Express
+            </td>
+            <td>
+              20:10
+            </td>
+            <br></br>
+            <td>
+              07:15
+            </td>
+          </tr>
+          <tr>
+            <td>
+              16727
+            </td>
+            <td>
+              Pandian Express
+            </td>
+            <td>
+              22:15
+            </td>
+            <br></br>
+            <td>
+              07:45
+            </td>
+          </tr>
+          <tr>
+            <td>
+              16747
+            </td>
+            <td>
+              Guruvayur Express
+            </td>
+            <td>
+              23:15
+            </td>
+            <br></br>
+            <td>
+              06:15
+            </td>
+          </tr>
+          <tr>
+            <td>
+              16573
+            </td>
+            <td>
+              Thiruchirapalli Express
+            </td>
+            <td>
+              18:45
+            </td>
+            <br></br>
+            <td>
+              08:20
+            </td>
+          </tr>
+          <tr>
+            <td>
+              12267
+            </td>
+            <td>
+              Duronto Express
+            </td>
+            <td>
+              12:40
+            </td>
+            <br></br>
+            <td>
+              19:15
+            </td>
+          </tr>
+          <tr>
+            <td>
+              16543
+            </td>
+            <td>
+              Rajdhani Express
+            </td>
+            <td>
+              17:15
+            </td>
+            <br></br>
+            <td>
+              23:15
+            </td>
+          </tr>
+          <tr>
+            <td>
+              15624
+            </td>
+            <td>
+              Shatbdi Express
+            </td>
+            <td>
+              07:15
+            </td>
+            <br></br>
+            <td>
+              19:15
+            </td>
+          </tr>
+          <tr>
+            <td>
+              19541
+            </td>
+            <td>
+              Gatimaan Express
+            </td>
+            <td>
+              04:15
+            </td>
+            <br></br>
+            <td>
+              15:15
+            </td>
+          </tr>
+          <tr>
+            <td>
+              13514
+            </td>
+            <td>
+              Humsafar Express
+            </td>
+            <td>
+              13:15
+            </td>
+            <br></br>
+            <td>
+              19:45
+            </td>
+          </tr>
+          <tr>
+            <td>
+              11654
+            </td>
+            <td>
+              Garib Rath Express
+            </td>
+            <td>
+              17:15
+            </td>
+            <br></br>
+            <td>
+              19:15
+            </td>
+          </tr>
+          <tr>
+            <td>
+              14521
+            </td>
+            <td>
+              Howrah Express
+            </td>
+            <td>
+              17:15
+            </td>
+            <br></br>
+            <td>
+              09:15
+            </td>
+          </tr>
+          <tr>
+            <td>
+              11102
+            </td>
+            <td>
+              Tejas Express
+            </td>
+            <td>
+              20:30
+            </td>
+            <br></br>
+            <td>
+              12:15
+            </td>
+          </tr>
+          <tr>
+            <td>
+              18452
+            </td>
+            <td>
+              Jaipur Superfast Express
+            </td>
+            <td>
+              08:15
+            </td>
+            <br></br>
+            <td>
+              19:15
+            </td>
+          </tr>
+          <tr>
+            <td>
+              19089
+            </td>
+            <td>
+              Chennai Express
+            </td>
+            <td>
+              06:15
+            </td>
+            <br></br>
+            <td>
+              19:15
+            </td>
+          </tr>
+          <tr>
+            <td>
+              13476
+            </td>
+            <td>
+              Goa Sampark Kranti Express
+            </td>
+            <td>
+              17:15
+            </td>
+            <br></br>
+            <td>
+              05:35
+            </td>
+          </tr>
+          <tr>
+            <td>
+              14587
+            </td>
+            <td>
+              Kanyakumari Express
+            </td>
+            <td>
+              17:15
+            </td>
+            <br></br>
+            <td>
+              12:50
+            </td>
+          </tr>
+          <br></br>
+          </tbody>
+        </table>
+        <p>
+            Any queries contact : <a href="https://indianrailways.gov.in/">Click Here</a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
